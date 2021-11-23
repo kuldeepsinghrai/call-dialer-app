ddf40d1aa7bbbe122dbf0de9e03784afb24d8a16
Kuldeep Singh Rai <imkuldeepsinghrai@gmail.com>
2021-11-23 16:45:20 +0530

    Initial Commit

--- app/src/main/java/com/ksr/phone_call_dialer_app/MainActivity.java
+++ app/src/main/java/com/ksr/phone_call_dialer_app/MainActivity.java
@@ -2,10 +2,22 @@ package com.ksr.phone_call_dialer_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
     TextView textNumber;
@@ -31,7 +43,125 @@ public class MainActivity extends AppCompatActivity {
         btn0 = findViewById(R.id.btn0);
         btnHash = findViewById(R.id.btnHash);
         btnCall = findViewById(R.id.btnCall);
+        textNumber = findViewById(R.id.textNumber);
 
 
+        Dexter.withContext(this).withPermission(Manifest.permission.CALL_PHONE).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        }).check();
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"9");
+            }
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"0");
+            }
+        });
+        btnStar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"*");
+            }
+        });
+        btnHash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textNumber.setText(textNumber.getText()+"#");
+            }
+        });
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeCall();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StringBuilder stringBuilder = new StringBuilder(textNumber.getText());
+                stringBuilder.deleteCharAt(textNumber.getText().length()-1);
+                String newTextNumberString = stringBuilder.toString();
+                textNumber.setText(newTextNumberString);
+            }
+        });
+
+
+    }
+
+
+    private void makeCall() {
+        String phoneNumber = textNumber.getText().toString();
+        String dial = "tel:"+phoneNumber;
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse(dial));
+        startActivity(intent);
     }
 }
